18638ab2e46144c4a42f1ea9256cda8d7f487232
Matias Nassi <matinas88@gmail.com>
2017-12-20 03:28:05 -0300

    Initial commit for SDL-ttf

--- openvrsimplexamples/src/main.cpp
+++ openvrsimplexamples/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <openvr.h>
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 using namespace std;
 using namespace vr;
@@ -22,29 +23,29 @@ int main(int argv, char** args) {
 
 	if (init_SDL() != 0) return -1;
 
-	if (VR_IsHmdPresent())
-	{
-		cout << "An HMD was successfully found in the system" << endl;
+	//if (VR_IsHmdPresent())
+	//{
+	//	cout << "An HMD was successfully found in the system" << endl;
 
-		if (VR_IsRuntimeInstalled()) {
-			const char* runtime_path = VR_RuntimePath();
-			cout << "Runtime correctly installed at '" << runtime_path << "'" << endl;
-		}
-		else
-		{
-			cout << "Runtime was not found, quitting app" << endl;
-			return -1;
-		}
-	}
-	else
-	{
-		cout << "No HMD was found in the system, quitting app" << endl;
-		return -1;
-	}
+	//	if (VR_IsRuntimeInstalled()) {
+	//		const char* runtime_path = VR_RuntimePath();
+	//		cout << "Runtime correctly installed at '" << runtime_path << "'" << endl;
+	//	}
+	//	else
+	//	{
+	//		cout << "Runtime was not found, quitting app" << endl;
+	//		return -1;
+	//	}
+	//}
+	//else
+	//{
+	//	cout << "No HMD was found in the system, quitting app" << endl;
+	//	return -1;
+	//}
 
-	// Load the SteamVR Runtime
-	vr_context = VR_Init(&err,EVRApplicationType::VRApplication_Scene);
-	vr_context == NULL ? cout << "Error while initializing SteamVR runtime. Error code is " << VR_GetVRInitErrorAsSymbol(err) << endl : cout << "SteamVR runtime successfully initialized" << endl;
+	//// Load the SteamVR Runtime
+	//vr_context = VR_Init(&err,EVRApplicationType::VRApplication_Scene);
+	//vr_context == NULL ? cout << "Error while initializing SteamVR runtime. Error code is " << VR_GetVRInitErrorAsSymbol(err) << endl : cout << "SteamVR runtime successfully initialized" << endl;
 
 	while (!app_end)
 	{
@@ -88,6 +89,14 @@ int init_SDL()
 	{
 		SDL_Log("Unable to create SDL Window: %s", SDL_GetError());
 		SDL_Quit();
+		return -1;
+	}
+
+	if (TTF_Init() != 0)
+	{
+		SDL_Log("Unable to initialize SDL ttf: %s", TTF_GetError());
+		SDL_Quit();
+
 		return -1;
 	}
 
